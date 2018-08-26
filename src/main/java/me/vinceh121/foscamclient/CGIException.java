1610b41bf0eefba0685eb330b1ec9e37cc6a0f13
vinceh121 <vincent.hyvert@sfr.fr>
2018-08-27 00:11:08 +0200

    Made CGI Exception

--- src/main/java/me/vinceh121/foscamclient/CGIException.java
+++ src/main/java/me/vinceh121/foscamclient/CGIException.java
@@ -8,12 +8,42 @@ public class CGIException extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	public CGIException(int result) {
+		super(getMessageForCode(result));
 		this.result = result;
-		
-		switch (result) { // TODO: Set message according to result code
+	}
+
+	public static String getMessageForCode(int code) {
+		String s = null;
+		switch (code) { // Those code have been copied directly from the documentation (yes, including the English mistakes)
 		case 0:
-			
+			s = "Success";
+			break;
+		case -1:
+			s = "CGI request string format error";
+			break;
+		case -2:
+			s = "Username or password error";
+			break;
+		case -3:
+			s = "Access deny";
+			break;
+		case -4:
+			s = "CGI execute fail";
+			break;
+		case -5:
+			s = "Timeout";
+			break;
+		case -6:
+			s = "Reserve";
+			break;
+		case -7:
+			s = "Unknown error";
+			break;
+		case -8:
+			s = "Reserve";
+			break;
 		}
+		return s;
 	}
 
 	public int getResult() {
